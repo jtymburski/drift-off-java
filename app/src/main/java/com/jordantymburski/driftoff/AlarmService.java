6597cafd8e70df9b921d3b7d280c4804646b44e6
Jordan Tymburski <hello@jordantymburski.com>
2019-05-09 21:05:46 -0600

    Implemented the new API for requesting audio focus for all devices on O+. Maintained the old functionality for legacy devices

--- app/src/main/java/com/jordantymburski/driftoff/AlarmService.java
+++ app/src/main/java/com/jordantymburski/driftoff/AlarmService.java
@@ -1,9 +1,13 @@
 package com.jordantymburski.driftoff;
 
+import android.annotation.TargetApi;
 import android.app.job.JobParameters;
 import android.app.job.JobService;
 import android.content.Context;
+import android.media.AudioAttributes;
+import android.media.AudioFocusRequest;
 import android.media.AudioManager;
+import android.os.Build;
 import android.util.Log;
 
 public class AlarmService extends JobService {
@@ -11,7 +15,11 @@ public class AlarmService extends JobService {
     public boolean onStartJob(JobParameters params) {
         AudioManager am = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
         if (am != null) {
-            am.requestAudioFocus(null, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                requestAudioFocus(am);
+            } else {
+                requestAudioFocusLegacy(am);
+            }
         } else {
             Log.e("DriftOff",
                     "Failed to get the audio manager to gain focus of the audio system!");
@@ -22,5 +30,28 @@ public class AlarmService extends JobService {
     @Override
     public boolean onStopJob(JobParameters params) {
         return false;
+    }
+
+    /* ==============================================
+     * INTERNAL FUNCTIONS
+     * ============================================== */
+
+    @TargetApi(Build.VERSION_CODES.O)
+    private void requestAudioFocus(AudioManager am) {
+        AudioAttributes attributes = new AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_MEDIA)
+                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                .build();
+
+        AudioFocusRequest focusRequest = new AudioFocusRequest.Builder(AudioManager.AUDIOFOCUS_GAIN)
+                .setAudioAttributes(attributes)
+                .build();
+
+        am.requestAudioFocus(focusRequest);
+    }
+
+    // deprecated on {@value Build.VERSION_CODES.O}
+    private void requestAudioFocusLegacy(AudioManager am) {
+        am.requestAudioFocus(null, AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN);
     }
 }
